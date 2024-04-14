2fc005b86602472d0996870d208fa936ab47b839
Arham <arhamansari454647@gmail.com>
2024-04-14 10:49:36 +0500

    Shadcn install and frontend started

---
+++ frontend/hackathon-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button>
+          Hello From Shadcn
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
