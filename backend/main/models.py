ec263fff8de52e255bba84e04b8891c630a05bfd
Mazen Mamdouh <mamdouhsalem10000@gmail.com>
2024-04-17 22:24:03 +0200

    Improved models
    Added an appointment model linked with the user through a many to many relationship, added more fields to the user profile model.

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,11 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Appointment(models.Model):
+    # TODO: Make a model for doctors and link it here (after the hackathon)
+    scheduled_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    doctor = models.CharField(max_length=150, null=True, blank=True)
+    time = models.DateTimeField(blank=True, null=True)
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(null=True, blank=True)
     first_name = models.CharField(verbose_name="First Name", max_length=150, null=True, blank=True)
     last_name = models.CharField(verbose_name="Last Name", max_length=150, null=True, blank=True)
+    disability = models.CharField(max_length=150, null=True, blank=True)
+    diagnosis = models.TextField(max_length=999, null=True, blank= True)
+    # TODO: Add a background task to delete the chat log once a while or when it reaches the max length (after the hackathon)
+    chat_log = models.TextField(max_length=99999, null=True, blank=True)
+    appointments = models.ManyToManyField(Appointment)
 
     @property
     def full_name(self):
