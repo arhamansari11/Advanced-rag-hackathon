f392cba68dba96e7f3cdef195ea1f758c450f98b
Mazen Mamdouh <mamdouhsalem10000@gmail.com>
2024-04-18 12:56:13 +0200

    Fixed Chatbot.jsx
    Now it works :D

--- frontend/hackathon-app/src/pages/Chatbot.jsx
+++ frontend/hackathon-app/src/pages/Chatbot.jsx
@@ -1,33 +1,17 @@
 import { ReactChatbot } from "@vectara/react-chatbot";
-import Logo from "../assets/images/equacare.png"
-
-const Chatbot = () => (
-  <>
-    <div className="container-fluid">
-  <div className="row">
-    <div className="col-12 text-center">
-      <h1 style={{ color: "#156082", fontWeight: "bold", fontSize: "40px" }}>
-        Equa Care
-      </h1>
-      <p style={{ fontSize: "20px" }}>
-        Providing equal access to healthcare for patients who find social
-        interactions challenging
-      </p>
-      <img src={Logo} alt="" style={{ width: "300px", height: "auto" }} />
-    </div>
+export const Chatbot = () => (
+  <div>
+    <ReactChatbot
+      customerId="29746375"
+      corpusIds={["3"]}
+      apiKey="zut_AcXkxxBdbcnL8hVb7n9sSK2ZDdjxFk6ukQsCvg"
+      title="Vectara Docs Chatbot"
+      placeholder='Try "What is Vectara?" or "How does RAG work?"'
+      inputSize="large"
+      enableStreaming={true}
+      language="eng"
+    />
   </div>
-</div>
-
-    <ReactChatbot title="Chat with EquaCare" placeholder="Type your Query?" />
-  </>
 );
-export default Chatbot;
 
-// customerId="29746375"
-// corpusIds="3"
-// apiKey="zqt_AcXkxwyKoj6M45VSTlSnizzu0WOVaDpRJCJ67Q"
-// title="Vectara Docs Chatbot"
-// placeholder='Try "What is Vectara?" or "How does RAG work?"'
-// inputSize="large"
-// enableStreaming={true}
-// language="eng"
+export default Chatbot
